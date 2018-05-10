49ed9e90e8cfd78b157a2b2498ee8d3948d5c99c
udasari <udasari@sargent-disc.com>
2018-05-10 14:30:08 +0100

    Fixed HTTP code

--- src/test/java/com/org/api/CommonLogin.java
+++ src/test/java/com/org/api/CommonLogin.java
@@ -25,7 +25,7 @@ public abstract class CommonLogin {
     public static final String JSESSIONID = "JSESSIONID";
     public static final String XSRF_TOKEN = "XSRF-TOKEN";
     public static final String RESULTS = "results";
-    public static final String PASSWORD = "Cr3w_M3mb3rs";
+    public static final String PASSWORD = "SD_Crew_H1re";
     public static final String SECURITY_CODE = "1314";
     protected Response response = null;
 
